977697646e928573d66673e6266660466b3eca68
Riplis Martinkevicius <xwxxfx@gmail.com>
2023-06-26 15:19:40 +0300

    Clean up errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import MainLayout from "./layout/Homepage";
-import ImageGallery from "./layout/ImageGallery";
-import ImageCard from "./layout/ImageCard";
 
 const App: React.FC = () => {
   return (
     <MainLayout>
-      <ImageGallery>
+      {/* <ImageGallery>
         <ImageCard title="img 1" description="It's moss I guess" />
-      </ImageGallery>
+      </ImageGallery> */}
     </MainLayout>
   );
 };
